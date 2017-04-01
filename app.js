e812ff3fa6eb35266d1ab379a0ee95e15e9d7484
Tim Paik <tswpaik@gmail.com>
2017-04-01 23:28:17 +0000

    OS and window size styling refactor

--- app.js
+++ app.js
@@ -4,36 +4,12 @@ if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
 if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
 if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
-console.log(window.screen.availWidth);
-
-if (OSName === "Windows" || OSName === "UNIX" || OSName === "Linux") {
+if (OSName === "Windows" || OSName === "UNIX" || OSName === "Linux" && window.screen.availWidth < 1025) {
   $('.hidden').first().show();
   $('html, h1, .types').css('font-weight', '300');
 } else {
   $('.hidden').first().slideDown(1000);    
 }
-  
-
-$(function(){
-function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-      // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
-        return "Windows Phone";
-    }
-
-    if (/android/i.test(userAgent)) {
-        return "Android";
-    }
-
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "iOS";
-    }
-
-    return "unknown";
-}  
   
   $(document).on('click', '.open_close', function(e) {
     e.preventDefault();
