e6a501a23468e249ae87044ed808d365d87414c4
Tim Paik <tswpaik@gmail.com>
2017-04-09 20:16:26 +0000

    Readbility refactor

--- app.js
+++ app.js
@@ -7,26 +7,51 @@ $(function(){
   if (navigator.appVersion.indexOf("Linux") != -1) OS = "Linux";
   
   if (OS === "Windows" || OS === "UNIX" || OS === "Linux" || window.screen.availWidth < 1025) {
-    $('body, h1, .types').css('font-weight', '300');
+    $('body, h1').css('font-weight', '300');
     
     if (OS === "Windows" || OS === "UNIX" || OS === "Linux") {
+      $('body, h1').css('font-weight', '100');
+      $('#row1, #nav_text, .types, .skills, .app_name, .app_description').css('-webkit-font-smoothing', 'subpixel-antialiased');
+      $('#row1, #nav_text, .types, .skills, .app_name, .app_description').css('-webkit-text-stroke', '0.2px');
+      $('.fa').css('-webkit-text-stroke', '0');
+      $('.hash_section, .skills_description, .app_details').css('font-family', "'Source Sans Pro', sans-serif");
+      $('.hash').css('font-family', "'Lato', sans-serif");
+      $('#row1, #nav_text').css('text-shadow','0 0 5spx #888888');  
       $('#profilepic').css('-webkit-filter', 'brightness(1.1) contrast(1.2) grayscale(0.3)');
       $('#profilepic').css('filter', 'brightness(1.1) contrast(1.2) grayscale(0.3)');
+      $('.icon_border').css('border-bottom','2px solid rgba(255, 255, 255, 0.15)');
+      $('.hash_section').css('border-left','2px solid rgba(255, 255, 255, 0.15)');
+      $('.types').css('border-right','2px solid rgba(255, 255, 255, 0.15)');
+      $('.skills_description').css('border-top','2px solid rgba(255, 255, 255, 0.15)');
+      $('.app_description').css('border-left','2px solid rgba(255, 255, 255, 0.15)');
+      $('.app_section').css('border-top','2px solid rgba(255, 255, 255, 0.15)');
     }
   }
 
-  if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-    $('#my_name').css('text-shadow', '0 0 2px #555555'); 
-    $('#nav_text, .hash').css('font-weight', '300');  
-    $('.types, .skills, .skills_description').css('opacity', '1');
+  if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent)) {
+    $('body, h1').css('font-weight', '300');
+    $('.transparent').css('font-size', '28px');
+    $('.skills').css('font-size', '34px');
+    $('#row2, #row4').css('background-attachment', 'scroll');
+  }
 
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    $('#row2, #row3, #row4').css('background-attachment', 'scroll');
+  }
+
+  if (window.screen.availWidth > 1024 && navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+    $('#row1, #nav_text, .hash, .punc').css('-webkit-text-stroke', '0.5px');
+    $('#row1, #nav_text').css('text-shadow','0 0 1px #555555');  
+    $('#my_name').css('text-shadow','0 0 1px #666666');  
+    $('.fa-hand-peace-o, .contact_icon').css('-webkit-text-stroke', '0');
+    $('#description2, #description3, #description4').css('opacity', '1');
   }
 
   if ($(window).width() > 767) {
     let height = $("#row1").outerHeight(true);    
     
     $(window).scroll(function(){                          
-      if ($(window).scrollTop() > (height - 60)){
+      if ($(window).scrollTop() > (height - 55)){
           $('nav').fadeIn("slow");
       } else {
           $('nav').fadeOut("fast");
@@ -45,6 +70,11 @@ $(function(){
   $('#scroll_up').click(function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, "slow");
+  });
+
+  $('.contact_link').hover(function() {
+    $(this).children('.contact_icon').toggleClass('icon_hover');
+    $(this).children('.icon_text').toggleClass('text_hover');
   });
 
   if (window.screen.availWidth > 768) {
@@ -72,9 +102,9 @@ $(function(){
     });
     
     $('.type_section').hover(function() {
+      $(this).toggleClass('highlight_type');
       $(this).children('.types').toggleClass('text_hover');
       $(this).children('.types').children('.types_icon').toggleClass('icon_hover');     
-      $(this).children('.types').siblings('.skills, .skills_description').toggleClass('text_hover');     
     });
   }
   
@@ -82,7 +112,6 @@ $(function(){
       $('.fa-chevron-down, .fa-chevron-up').toggleClass('icon_hover');
   }, 1000);
 
-console.log( $('.scroll').children('i'));
   $(document).on('click', '.to_top, .up', function (e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, "slow");
@@ -102,4 +131,5 @@ console.log( $('.scroll').children('i'));
     e.preventDefault();
     $('html, body').animate({ scrollTop: $('#row4').offset().top - 50 }, 'slow');
   });
+  
 });
